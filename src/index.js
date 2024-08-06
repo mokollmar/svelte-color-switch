5de6385a2c62b4e5ee9a878fd0124245c5732446
mokollmar <153505840+mokollmar@users.noreply.github.com>
2024-08-06 19:44:57 +0200

    solved vite export warning

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-export { default as SingleColorPicker } from './lib/SingleColorPicker/SingleColorPicker.svelte'
-export { default as MultiColorPicker } from './lib/MultiColorPicker/MultiColorPicker.svelte'
-export { default as ColorDelay } from './lib/ColorDelay/ColorDelay.svelte'
+
+    export { default as SingleColorPicker } from './lib/SingleColorPicker/SingleColorPicker.svelte'
+    export { default as MultiColorPicker } from './lib/MultiColorPicker/MultiColorPicker.svelte'
+    export { default as ColorDelay } from './lib/ColorDelay/ColorDelay.svelte'
